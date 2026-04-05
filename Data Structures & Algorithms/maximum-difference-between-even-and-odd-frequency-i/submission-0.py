fd66bec53f0222f80a6f3af81eea962b4d7621c2
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-04-06 00:27:47 +0530

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-0

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        m={}
+        mo=me=0
+        for i in s:
+            m[i]=m.get(i,0)+1
+        for i,j in m.items():
+            if j%2==1:
+                mo=max(mo,j)
+            else:
+                me=max(me,j)
+        return abs(mo-me)
+
+        
